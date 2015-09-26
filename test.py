d8060c097a4eccf5f5d7a20d3152ad268b6b7393
Michael Gajda <m@giiduh.com>
2015-09-26 13:51:04 -0400

    This is a base commit for trumpify.

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import trumpify as t
+import urllib
 
 app = Flask(__name__)
 model = t.make_model()
@@ -14,11 +15,19 @@ def my_form():
 def my_form_post():
 
     text = request.form['text']
-    #change this to trumpify
-    #processed_text = text.upper()
-    #return processed_text
-    return t.trumpify(text, model)
+    output = t.trumpify(text.lower(), model)
+
+    return render_template("test.html", input=text.lower(), output=output, url=urllib.quote_plus(output))
+
+@app.route('/trumpified/')
+def my_test():
+
+ #   text = request.form['text']
+ 	text = request.args.get('text')
+	return(t.trumpify(text.lower(), model))
+   # return render_template("test.html", output=output)
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
--- trumpify.py
+++ trumpify.py
@@ -6,6 +6,8 @@ def make_model():
 	data = []
 	errors = 0
 
+	punctuation = """!@#$%^&*(){}[]:;"',.<>/?"""
+
 	with open("speech3.txt", "r") as speech:
 		for line in speech:
 			try:
@@ -14,8 +16,8 @@ def make_model():
 					tokenized_sentence = nltk.word_tokenize(sentence)
 					final = []
 					for word in tokenized_sentence:
-						if word not in nltk.corpus.stopwords.words("english"):
-							final += [word]
+						if word not in nltk.corpus.stopwords.words("english") and word not in punctuation:
+							final += [pattern.en.singularize(word)]
 					data += [final]
 
 			except UnicodeDecodeError:
@@ -29,33 +31,50 @@ def make_model():
 def trumpify(input_sentence, model, verbose=False, pos_to_search_for=["NN", "JJ"]):
 	final_output = ""
 
+	punctuation = """!@#$%^&*(){}[]:;"',.<>/?"""
+
 	for word, pos in pattern.en.tag(input_sentence):
+
+		singular_form = pattern.en.singularize(word)
+		singular = singular_form == word
 		
 		if pos[:2] in pos_to_search_for:
 			try:
-				sim = model.most_similar(positive=[word, "bad"])
+				sim = model.most_similar(positive=[singular_form])
 				changed = False
 
 				for top_word, cosin_sim in sim:
 					new_word, new_pos = pattern.en.tag(top_word)[0]
-					if pos[:2] in pos_to_search_for:
+					if new_pos == pos:
 						if verbose == True:
 							print "changed: ", word, top_word, pos
-						final_output += new_word + " "
+						if singular == True:
+							final_output += new_word + " "
+						else: #needs to be pluralized
+							final_output += pattern.en.pluralize(new_word, pos=pos)
 						changed = True
 						break #this doesn't seem to work
 
 				#if we make it to here, we must add the word to the string
 				if changed == False: #needed to do this to get it to work if the word was not found in sim
 					final_output += word + " "
-			except KeyError:
+			except KeyError: #if it is not in the model, then go here
 				if verbose == True:
 					print word, " not found in corpus"
-				final_output += word + " "
+				if word not in punctuation:
+					final_output += word + " "
+				else:
+					final_output = final_output[:-1]
+					final_output += word + " "
 		else:
 			if verbose == True:
 				print word, "not the proper pos", pos
-			final_output += word + " "
+
+			if word not in punctuation:
+				final_output += word + " "
+			else:
+				final_output = final_output[:-1]
+				final_output += word + " "
 	if verbose == True:
 		print "input: ", input_sentence
 		print "output: ", final_output
